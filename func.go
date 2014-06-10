424366cb7650a46b909aaa398970da833fe8af37
Marco Singer <markaum@gmail.com>
2014-06-10 17:21:04 -0300

    added more variable examples

--- func.go
+++ func.go
@@ -4,6 +4,14 @@ import "fmt"
 
 // start func OMIT
 func main() {
+	// Existem 3 formas de declarar uma variável:
+
+	// var message string
+	// message = Hello("Rocky Balboa")
+
+	// var message = Hello("Rocky Balboa")
+
+	// essa seria uma sugar syntax (somente dentro de funções)
 	message := Hello("Rocky Balboa")
 	fmt.Println(message)
 }
